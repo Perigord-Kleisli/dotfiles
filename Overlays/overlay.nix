92574436d910554afed96adbfd94c74634bcc0f3
Trouble-Truffle <perigordtruffle7318@gmail.com>
2023-01-06 06:21:04 +0800

    Added marksman overlay from nixpkgs pr: 193682

--- Overlays/overlay.nix
+++ Overlays/overlay.nix
@@ -2,7 +2,7 @@ self: super:
 {
   zplug-overlay = super.callPackage ./Zplug.nix {};
   raleway-overlay = super.callPackage ./Raleway.nix {};
-  marksman = super.callPackage ./marksman.nix {};
+  marksman = super.callPackage ./marksman {};
   fennel-ls = super.callPackage ./fennel-ls.nix {};
   fennel-language-server = super.callPackage ./fennel-language-server.nix {};
 }
